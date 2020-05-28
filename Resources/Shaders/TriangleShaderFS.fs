8268ce558369500845dca9d59cbd2aec0a3f83b0
blinfoldking <ganeshadanu1@gmail.com>
2020-05-28 16:50:40 +0700

    change rainbow pattern

--- Resources/Shaders/TriangleShaderFS.fs
+++ Resources/Shaders/TriangleShaderFS.fs
@@ -4,6 +4,9 @@ varying lowp vec4 posL;
 void main()
 {
 	vec3 position = posL.xyz;
-	vec3 color = position + vec3(0.5, 0.5, 0.5);
+	vec3 color;
+	color.r = - posL.x + 0.5;
+	color.g = abs(posL.x + posL.y);
+	color.b = - posL.y + 0.5;
 	gl_FragColor = vec4(color, 1.0);
 }
